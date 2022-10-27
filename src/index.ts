4c9c2e7574c9e9bb98edd531af3f42bb51fb024f
Thomas Boullier <thomas.boullier@marklix.com>
2022-10-27 14:34:54 +0200

    Refactor main files

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,45 @@
-import { Plugin } from "vue";
+/*
+ * MARKLIX PLANNER
+ * Copyright (c) 2022, Marklix SAS.
+ *
+ * Tous droits réservés pour tous pays. L’ensemble du contenu de ce fichier est confidentiel et demeure la propriété
+ * exclusive de son auteur. Tous les droits sont réservés pour tous pays, notamment les droits de consultation, de
+ * reproduction, de représentation, d’adaptation, de modification, de traduction, de distribution, de
+ * commercialisation, d’usage, d’exploitation et de cession dudit fichier. Les traductions éventuelles de cette notice
+ * sont uniquement données à titre indicatif.
+ *
+ * All rights reserved for all countries. All information contained herein is confidential and remain the exclusive
+ * property of its author. All rights are reserved for all countries, including the rights to read, copy, depict,
+ * adapt, modify, translate, distribute, sell, use (including commercially) and concede this file. Only the French
+ * version of this notice is legally binding (see above).
+ */
+
+// Import dayjs and its plugins
 import dayjs from "dayjs";
+
 import isSameOrBefore from "dayjs/plugin/isSameOrBefore";
 import isSameOrAfter from "dayjs/plugin/isSameOrAfter";
 import isBetween from "dayjs/plugin/isBetween";
-import customParseFormat from "dayjs/plugin/customParseFormat";
 
-import GGanttChart from "./components/GGanttChart.vue";
-import GGanttRow from "./components/GGanttRow.vue";
+import customParseFormat from "dayjs/plugin/customParseFormat";
 
 dayjs.extend(isSameOrBefore);
 dayjs.extend(isSameOrAfter);
 dayjs.extend(isBetween);
 dayjs.extend(customParseFormat);
 
-const ganttastic: Plugin = {
-  install(app, options?) {
+// Create plugin
+
+import { Plugin } from "vue";
+
+import GGanttChart from "./components/GGanttChart.vue";
+import GGanttRow from "./components/GGanttRow.vue";
+
+const planner: Plugin = {
+  install(app) {
     app.component("GGanttChart", GGanttChart);
     app.component("GGanttRow", GGanttRow);
   },
 };
 
-export default ganttastic;
+export default planner;
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,23 @@
-import { createApp } from "vue";
+/*
+ * MARKLIX PLANNER
+ * Copyright (c) 2022, Marklix SAS.
+ *
+ * Tous droits réservés pour tous pays. L’ensemble du contenu de ce fichier est confidentiel et demeure la propriété
+ * exclusive de son auteur. Tous les droits sont réservés pour tous pays, notamment les droits de consultation, de
+ * reproduction, de représentation, d’adaptation, de modification, de traduction, de distribution, de
+ * commercialisation, d’usage, d’exploitation et de cession dudit fichier. Les traductions éventuelles de cette notice
+ * sont uniquement données à titre indicatif.
+ *
+ * All rights reserved for all countries. All information contained herein is confidential and remain the exclusive
+ * property of its author. All rights are reserved for all countries, including the rights to read, copy, depict,
+ * adapt, modify, translate, distribute, sell, use (including commercially) and concede this file. Only the French
+ * version of this notice is legally binding (see above).
+ */
+
+import planner from "./index";
+
 import PlannerDemo from "./PlannerDemo.vue";
-import dayjs from "dayjs";
-import isSameOrBefore from "dayjs/plugin/isSameOrBefore";
-import isSameOrAfter from "dayjs/plugin/isSameOrAfter";
-import isBetween from "dayjs/plugin/isBetween";
-import customParseFormat from "dayjs/plugin/customParseFormat";
-import ganttastic from "./index";
 
-dayjs.extend(isSameOrBefore);
-dayjs.extend(isSameOrAfter);
-dayjs.extend(isBetween);
-dayjs.extend(customParseFormat);
+import { createApp } from "vue";
 
-createApp(PlannerDemo).use(ganttastic).mount("#app");
+createApp(PlannerDemo).use(planner).mount("#app");
